282fadd03635c87f9b01e1542e8aa75666ff9b4e
Łukasz Borucki <lukasz@borucki.co>
2017-08-07 22:01:38 +0200

    Create Skills Fragment class, fragment skills layout, akills technologies single ror layout, skills traits single row layout
    * Created Skills Fragment - class
    * Created fragment skills - layout
    * Created skills technologies single row - layout
    * Created skills traits single row - layout

---
+++ app/src/main/java/co/borucki/mycv/view/fragments/SkillsFragment.java
@@ -0,0 +1,28 @@
+package co.borucki.mycv.view.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.borucki.mycv.R;
+
+/**
+ * Created by Lukasz on 2017-08-07.
+ */
+
+public class SkillsFragment extends Fragment {
+    public SkillsFragment() {
+    }
+    public static SkillsFragment newInstance(){
+        return new SkillsFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_skills, container, false);
+    }
+}
